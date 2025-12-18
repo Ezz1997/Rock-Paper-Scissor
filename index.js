6b463dfdf005742873157f745a3822a5c0b8408c
Ezz1997 <ezmarie97@gmail.com>
2025-12-18 18:52:52 +0200

    Write the logic to play a single round: playRound function

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function getComputerChoice(){
         return "rock";
     }else if(randomNum === 1){
         return "paper";
-    }else{
+    }else if(randomNum){
         return "scissors";
     }
 }
@@ -18,5 +18,52 @@ function getHumanChoice(){
     return humanChoice;
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice){
+
+    switch(humanChoice){
+        case "rock":
+            if(computerChoice === "paper"){
+                console.log("You Lose! Paper beats Rock");
+                computerScore++;
+            }else if(computerChoice === "rock"){
+                console.log("A Tie!");
+            }else if(computerChoice === "scissors"){
+                console.log("You Win! Rock beats Scissors");
+                humanScore++;
+            }
+
+            break;
+
+        case "paper":
+            if(computerChoice === "rock"){
+                console.log("You Win! Paper beats Rock");
+                humanScore++; 
+            }else if(computerChoice === "paper"){
+                console.log("A Tie!");
+            }else if(computerChoice === "scissors"){
+                console.log("You Lose! Scissors beats Paper");
+                computerScore++;
+            }
+
+            break;
+
+        case "scissors":
+            if(computerChoice === "rock"){
+                console.log("You Lose! Rock beats Scissors");
+                computerScore++; 
+            }else if(computerChoice === "paper"){
+                console.log("You Win! Scissors beats Paper");
+                humanScore++;
+            }else if(computerChoice === "scissors"){
+                console.log("A Tie!");
+            }
+
+            break;
+    }
+}
+
+const humanChoice = getHumanChoice().toLowerCase();
+const computerChoice = getComputerChoice().toLowerCase();
+
+playRound(humanChoice, computerChoice);
+
