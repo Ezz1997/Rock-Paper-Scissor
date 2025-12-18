1a040c245980ca620cdc592487200de69fde8de3
Ezz1997 <ezmarie97@gmail.com>
2025-12-18 18:12:20 +0200

    Write a function to randomly generate 1 of 3 options: rock, paper or scissors

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+function getComputerChoice(){
+    const randomNum = Math.floor(Math.random() * 3);
+    
+    if(randomNum === 0){
+        return "rock";
+    }else if(randomNum === 1){
+        return "paper";
+    }else{
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
